6f5ac652961bad263821161af67180780b062334
Cedric <cedric.frachisse@gmail.com>
2024-01-22 10:11:38 +0100

    premiere exp

---
+++ vue/exercice_my_first_app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Component } from 'vue';
+/*  
+CE COMPOSANT RACINE APP RESTE EN MODE COMPOSITION API
+IMPORTER LES COMPOSANTS ICI 
+*/
+
+</script>
+
+<template>
+  <h1>Ok</h1>
+  <h2>{{ name }}</h2>
+  <component/>
+  <Component/>
+  <!-- Monter les composants ici -->
+</template>
+
+<style scoped>
+
+
+</style>
