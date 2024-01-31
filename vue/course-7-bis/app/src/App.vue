24f0af260ee5c6695b856f2294f4c44bacdb0f7c
Cedric <cedric.frachisse@gmail.com>
2024-01-31 16:50:23 +0100

    1erjour routeur vuejs course-7

---
+++ vue/course-7-bis/app/src/App.vue
@@ -0,0 +1,63 @@
+<script >
+import MainLayout from './components/Layout/MainLayout.vue'
+import MainNav from './components/Layout/MainNav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainLayout,
+    MainNav
+  },
+  data() {
+    return {
+      userNavItemsArray : [
+        {
+          name: "Home",
+          link: "/",
+          target: "_self"
+        }
+      ],
+      navItemsArray : [
+        {
+          name: "Home",
+          link: "/",
+          target: "_self",
+          class: "link-body-emphasis"
+        },
+        {
+          name: "Products",
+          link: "/admin/products",
+          target: "_self",
+          class: "link-body-emphasis"
+        },
+        {
+          name: "LogIn",
+          link: "/LogIn",
+          target: "_self",
+          class: "link-body-emphasis"
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Monter les composants ici -->
+  <main-layout>
+    <template #header>
+      <main-nav
+        :navItems="navItemsArray"
+        :userNavItems="userNavItemsArray"
+        :showUserNav="true"
+      />
+    </template>
+    
+    <router-view></router-view>
+
+  </main-layout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/course-7-bis/app/src/views/LogIn.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        name: 'LogIn',
+    }
+</script>
+
+<template>
+    <section>
+        <form >
+            <label class="" for="login">Votre identifiant</label>
+            <input type="text"/>
+            <input type="submit" value="Envoyer le formulaire"/>
+        </form>
+    </section>
+</template>
+
+<style scoped>
+
+    
+
+</style>
